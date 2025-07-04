1915b9b1977feade0993ed03e48a09c23f45f8cd
Leandro <leandro.silva343@fatec.sp.gov.br>
2025-07-04 18:27:22 -0300

    style: aplica responsividade com PrimeFlex

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 import './App.css';
 import Busca from './components/Busca';
 
 function App() {
   return (
-    <div className="p-m-4">
-      <h2>Previsao do tempo</h2>
+    <div className="p-d-flex p-jc-center p-ai-center p-flex-column p-p-4" style={{ minHeight: '100vh' }}>
+      <h2 className="p-mb-3">previsão do tempo</h2>
       <Busca />
     </div>
   );
