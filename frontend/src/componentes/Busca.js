2eaeac78afb4a503f27cfbb75a2932d65777eedd
Leandro <leandro.silva343@fatec.sp.gov.br>
2025-07-04 17:22:53 -0300

    feat: implementa opções de escolha entre temperatura e pressão com RadioButtons

--- frontend/src/componentes/Busca.js
+++ frontend/src/componentes/Busca.js
@@ -1,7 +1,9 @@
 import { useState } from 'react';
+import { RadioButton } from 'primereact/radiobutton';
 
 function Busca() {
   const [coordenadas, setCoordenadas] = useState('-23.561167625063238, -46.65648357473211');
+  const [opcao, setOpcao] = useState('');
 
   return (
     <div className="p-mt-4">
@@ -12,6 +14,30 @@ function Busca() {
         placeholder="Latitude, Longitude"
         className="p-inputtext"
       />
+
+      <div className="p-formgroup-inline p-mt-2">
+        <div className="p-field-radiobutton">
+          <RadioButton
+            inputId="temp"
+            name="tipo"
+            value="temp"
+            onChange={(e) => setOpcao(e.value)}
+            checked={opcao === 'temp'}
+          />
+          <label htmlFor="temp">temperatura</label>
+        </div>
+
+        <div className="p-field-radiobutton">
+          <RadioButton
+            inputId="pressao"
+            name="tipo"
+            value="pressao"
+            onChange={(e) => setOpcao(e.value)}
+            checked={opcao === 'pressao'}
+          />
+          <label htmlFor="pressao">Pressao e umiedade</label>
+        </div>
+      </div>
     </div>
   );
 }
